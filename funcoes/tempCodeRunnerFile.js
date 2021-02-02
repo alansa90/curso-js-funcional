06323fca228d0618612ed9ea66d0f3b2ad0fa467
alan <alkinsret@gmail.com>
2021-02-02 19:46:19 -0300

    callback - reduce

---
+++ funcoes/tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+const media = carrinho
+//   .filter(item => item.fragil)
+//   .map(item => item.qtde * item.preco)
+//   .reduce((acc, el, i, array) => {
+//     const novaQtde = acc.qtde + 1
+//     const novoTotal = acc.total + el
+//     return {
+//       qtde: novaQtde,
+//       total: novoTotal,
+//       media: novoTotal/novaQtde
+//     }
+//   }, { qtde: 0, total: 0, media: 0})
