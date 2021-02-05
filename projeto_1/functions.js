137521bc9c8f9ff794b57bd723f9cbb5c649b32a
alan <alkinsret@gmail.com>
2021-02-05 15:16:24 -0300

    project one finished

--- projeto_1/functions.js
+++ projeto_1/functions.js
@@ -14,9 +14,9 @@ function lerPasta (caminho) {
   })
 }
 
-function elementosTerminadosCom (array, padrao) {
+function elementosTerminadosCom (array, padraoTextual) {
 
-  return array.filter(item => item.endsWith(padrao))
+  return array.filter(item => item.endsWith(padraoTextual))
 
 }
 
@@ -35,8 +35,72 @@ function lerArquivo (caminho) {
 function lerArquivos (caminhos) {
   return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
 }
+
+function removerElementosSeVazio (array) {
+  return array.filter(item => item.trim())
+}
+
+function removerElementosSeIncluir (padraoTextual, array) {
+  return function (array) {
+    return array.filter(item => !item.includes(padraoTextual))
+  }
+}
+
+function removerElementosSeApenasNumeros (array) {
+  return array.filter(item => {
+    const num = parseInt(item.trim())
+    return num !== num
+  })
+}
+
+function removerSimbolos (simbolos, array) {
+  return function (array) {
+    return array.map(item => {
+      let newText = item
+      simbolos.forEach(simbolo => {
+        newText = newText.split(simbolo).join('')
+      })
+      return newText
+    })
+  }
+}
+
+function mesclarElementos (array)  {
+  return array.join(' ')
+}
+
+function separarTextoPor (simbolo) {
+  return function (texto) {
+    return texto.split(simbolo)
+  }
+}
+
+function agruparElementos (palavras) {
+  return Object.values(palavras.reduce((acc, palavra) => {
+    const p = palavra.toLowerCase()
+    const qtde = acc[p] ? acc[p].qtde + 1 : 1
+    acc[p] = { elemento: p, qtde }
+    return acc
+  }, {}))
+}
+
+function ordenarPorAtributoNumerico (attr, ordem = 'asc') {
+  return function (array) {
+    const asc = (o1, o2) => o1[attr] - o2[attr]
+    const desc = (o1, o2) => o2[attr] - o1[attr]
+    return array.sort(ordem === 'asc' ? asc : desc)
+  }
+} 
 module.exports = {
   lerPasta,
   elementosTerminadosCom,
-  lerArquivos
+  lerArquivos,
+  removerElementosSeVazio,
+  removerElementosSeIncluir,
+  removerElementosSeApenasNumeros,
+  removerSimbolos,
+  mesclarElementos,
+  separarTextoPor,
+  agruparElementos,
+  ordenarPorAtributoNumerico
 }
--- projeto_1/index.js
+++ projeto_1/index.js
@@ -2,8 +2,32 @@ const path = require('path')
 const fn = require('./functions')
 
 const caminho = path.join(__dirname, '..', 'dados', 'legendas')
+const simbolos = [
+  '.', '?', '!', '-', ',', '"', '♪', '_', '<i>', '</i>', '\r', '[', ']', '(', ')'
+]
+
+const agruparElementos = (palavras) => {
+  return Object.values(palavras.reduce((acc, palavra) => {
+    const p = palavra.toLowerCase()
+    const qtde = acc[p] ? acc[p].qtde + 1 : 1
+    acc[p] = { elemento: p, qtde }
+    return acc
+  }, {}))
+}
 
 fn.lerPasta(caminho)
   .then(arquivos => fn.elementosTerminadosCom(arquivos, '.srt'))
   .then(caminhos => fn.lerArquivos(caminhos))
+  .then(fn.mesclarElementos)
+  .then(fn.separarTextoPor('\n'))
+  .then(fn.removerElementosSeVazio)
+  .then(fn.removerElementosSeIncluir('-->'))
+  .then(fn.removerElementosSeApenasNumeros)
+  .then(fn.removerSimbolos(simbolos))
+  .then(fn.mesclarElementos)
+  .then(fn.separarTextoPor(' '))
+  .then(fn.removerElementosSeVazio)
+  .then(fn.removerElementosSeApenasNumeros)
+  .then(fn.agruparElementos)
+  .then(fn.ordenarPorAtributoNumerico('qtde', 'desc'))
   .then(console.log)
