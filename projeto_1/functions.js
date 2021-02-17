ed9db66beaee7ee18e630742592728625c559ac2
alan <alkinsret@gmail.com>
2021-02-17 13:58:45 -0300

    composition

--- projeto_1/functions.js
+++ projeto_1/functions.js
@@ -1,103 +1,118 @@
 const fs = require('fs')
 const path = require('path')
 
-function lerPasta (caminho) {
-  return new Promise ((resolve, reject) => {
-    try {
-      const arquivos = fs.readdirSync(caminho).map(arquivo => path.join(caminho, arquivo))
-      resolve(arquivos)
+function composicao(...fns) {
+    return function(valor) {
+        return fns.reduce(async (acc, fn) => {
+            if(Promise.resolve(acc) === acc) {
+                return fn(await acc)
+            } else {
+                return fn(acc)
+            }
+        }, valor)
     }
-    catch (e) {
-      reject(e)
-    }
-  })
 }
 
-function elementosTerminadosCom (array, padraoTextual) {
-
-  return array.filter(item => item.endsWith(padraoTextual))
-
+function lerDiretorio (caminho) {
+    return new Promise((resolve, reject) => {
+        try {
+            const arquivos = fs.readdirSync(caminho).map(arquivo => {
+                return path.join(caminho, arquivo)
+            })
+            resolve(arquivos)
+        } catch (e) {
+            reject(e)
+        }
+    })
 }
 
 function lerArquivo (caminho) {
-  return new Promise((resolve,reject) => {
-    try {
-      const conteudo = fs.readFileSync(caminho, { encoding: 'utf-8'})
-      resolve(conteudo.toString())
-    }
-    catch (e) {
-      reject(e)
-    }
-  })
+    return new Promise((resolve, reject) => {
+        try {
+            const conteudo = fs.readFileSync(caminho, { encoding: 'utf-8' })
+            resolve(conteudo.toString())
+        } catch (e) {
+            reject(e)
+        }
+    })
 }
 
 function lerArquivos (caminhos) {
-  return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
+    return Promise.all(caminhos.map(caminho => lerArquivo(caminho)))
+}
+
+function elementosTerminadosCom (padraoTextual) {
+    return function (array) {
+        return array.filter(el => el.endsWith(padraoTextual))
+    }
 }
 
 function removerElementosSeVazio (array) {
-  return array.filter(item => item.trim())
+    return array.filter(el => el.trim())
 }
 
-function removerElementosSeIncluir (padraoTextual, array) {
-  return function (array) {
-    return array.filter(item => !item.includes(padraoTextual))
-  }
+function removerElementosSeIncluir (padraoTextual) {
+    return function (array) {
+        return array.filter(el => !el.includes(padraoTextual))
+    }
 }
 
-function removerElementosSeApenasNumeros (array) {
-  return array.filter(item => {
-    const num = parseInt(item.trim())
-    return num !== num
-  })
-}
-
-function removerSimbolos (simbolos, array) {
-  return function (array) {
-    return array.map(item => {
-      return simbolos.reduce((acc, simbolo) => {
-        return acc.split(simbolo).join('')
-      }, item)
+function removerElementosSeApenasNumero (array) {
+    return array.filter(el => {
+        const num = parseInt(el.trim())
+        return num !== num
     })
-  }
 }
 
-function mesclarElementos (array)  {
-  return array.join(' ')
+function removerSimbolos (simbolos) {
+    return function (array) {
+        return array.map(el => {
+            return simbolos.reduce((acc, simbolo) => {
+                return acc.split(simbolo).join('')
+            }, el)
+        })
+    }
+}
+
+function mesclarElementos (array) {
+    return array.join(' ')
 }
 
 function separarTextoPor (simbolo) {
-  return function (texto) {
-    return texto.split(simbolo)
-  }
+    return function (texto) {
+        return texto.split(simbolo)
+    }
 }
 
 function agruparElementos (palavras) {
-  return Object.values(palavras.reduce((acc, palavra) => {
-    const p = palavra.toLowerCase()
-    const qtde = acc[p] ? acc[p].qtde + 1 : 1
-    acc[p] = { elemento: p, qtde }
-    return acc
-  }, {}))
+    return Object.values(palavras.reduce((acc, palavra) => {
+        const el = palavra.toLowerCase()
+        const qtde = acc[el] ? acc[el].qtde + 1 : 1
+        acc[el] = { elemento: el, qtde }
+        return acc
+    }, {}))
 }
 
-function ordenarPorAtributoNumerico (attr, ordem = 'asc') {
-  return function (array) {
-    const asc = (o1, o2) => o1[attr] - o2[attr]
-    const desc = (o1, o2) => o2[attr] - o1[attr]
-    return [...array].sort(ordem === 'asc' ? asc : desc)
-  }
-} 
+function ordernarPorAtribNumerico (attr, ordem = 'asc') {
+    return function (array) {
+        const asc = (o1, o2) => o1[attr] - o2[attr]
+        const desc = (o1, o2) => o2[attr] - o1[attr]
+        return [...array].sort(ordem === 'asc' ? asc : desc)
+    }
+}
+
 module.exports = {
-  lerPasta,
-  elementosTerminadosCom,
-  lerArquivos,
-  removerElementosSeVazio,
-  removerElementosSeIncluir,
-  removerElementosSeApenasNumeros,
-  removerSimbolos,
-  mesclarElementos,
-  separarTextoPor,
-  agruparElementos,
-  ordenarPorAtributoNumerico
+    composicao,
+    lerDiretorio,
+    lerArquivo,
+    lerArquivos,
+    elementosTerminadosCom,
+    removerElementosSeVazio,
+    removerElementosSeIncluir,
+    removerElementosSeApenasNumero,
+    removerSimbolos,
+    mesclarElementos,
+    separarTextoPor,
+    agruparElementos,
+    ordernarPorAtribNumerico
 }
